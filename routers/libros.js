7e836a6d1a32a22f787999cf4f1eb9139c9731df
Melodi <melodimarisel@gmail.com>
2021-01-07 23:43:28 -0300

    tp1 grupo A cambios mel

--- routers/libros.js
+++ routers/libros.js
@@ -1,8 +1,48 @@
 var express = require('express');
 var router = express.Router();
 
+// Load the MySQL pool connection
+const pool = require('../app.js');
+
+/* Display all users
+router.get('/libro', (request, response) => {
+  pool.query('SELECT * FROM libros', (error, result) => {
+      if (error) throw error;
+
+      response.status(200).send(result, "Error inesperado");
+  });
+});
+*/
+router.get('/libro', async (req, res) => {
+
+  try {
+
+    const connection = await pool.createConnection();
+
+    try {
+      const sql = "SELECT * FROM libros ";
+      const flags = await connection.query(sql);
+
+      if (flags.length === 0)
+        return res.status(413).send({ message: 'Error inesperado' });
+
+      return res.status(200).send({"resultado":flags});
+
+    } finally {
+      pool.releaseConnection(connection);
+    }
+
+  } catch (e) {
+    console.error(e.message);
+    res.status(413).send({"Error": e.message});
+  }
+});
+
+
+module.exports = router;
+
 //GET '/libro' devuelve 200 y [{id: numero, nombre:string, descripcion:string, categoria_id:numero, persona_id:numero/null}] o bien 413, {mensaje: <descripcion del error>} "error inesperado"
-router.get('/libro', async (req, res)=>{
+/*router.get('/libro', async (req, res)=>{
 
  
   try{
@@ -15,6 +55,5 @@ router.get('/libro', async (req, res)=>{
       res.status(413).send({"Error": e.message});
   }
 
-});
+});*/
 
-module.exports = router;
